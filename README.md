2500672744a5f2e906f995125c632ac830a373e4
Finn Pedersen <finnpedersenfrance@gmail.com>
2022-01-31 12:03:17 +0100

    added github pages

--- README.md
+++ README.md
@@ -5,6 +5,10 @@ A small example to illustrate state, actions, messages and visualization in Elm.
 ![Screenshot](public/Screenshot.png)
 
 
+Try out the app here
+https://finnpedersenkazes.github.io/elm-toggle/
+
+
 # Create Elm App
 
 This project is bootstrapped with [Create Elm App](https://github.com/halfzebra/create-elm-app).
--- src/Main.elm
+++ src/Main.elm
@@ -47,8 +47,7 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ h1 [] [ text "Your Elm App is working!" ]
         ,   div []
             [ button [ onClick Toggle ] [ text "Toggle" ]
             , h1 [] [ text (viewLanguage model) ]
