7713ccf1efe0c2ba1c9adba2b850b29c6d63b999
Finn Pedersen <finnpedersenfrance@gmail.com>
2022-01-31 11:56:10 +0100

    Updates

---
+++ precache-manifest.ddc897090b9488f90b88a689c3fbec61.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf44d4bfebee62250c0aa30f06d65865",
+    "url": "/elm-toggle/Screenshot.png"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/elm-toggle/favicon.ico"
+  },
+  {
+    "revision": "0677cc131d46d63c71167a514f707907",
+    "url": "/elm-toggle/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/elm-toggle/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/elm-toggle/manifest.json"
+  },
+  {
+    "revision": "8c959267b2b8384c40a0",
+    "url": "/elm-toggle/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "8c959267b2b8384c40a0",
+    "url": "/elm-toggle/static/js/main.8c959267.chunk.js"
+  },
+  {
+    "revision": "29d9a0d805fcbe2b4973",
+    "url": "/elm-toggle/static/js/runtime~main.29d9a0d8.js"
+  },
+  {
+    "revision": "354e092f0aa2734f8f4c",
+    "url": "/elm-toggle/static/js/vendors~main.354e092f.chunk.js"
+  }
+]);
